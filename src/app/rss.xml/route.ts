bb2dd9b6d9fd77a1089b30626f44c41346d4d6da
Mats Larsen <mats@jun.codes>
2024-06-01 00:10:31 +0200

    feat: add rss.xml feed

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,33 @@
+import { getAllBlogs } from '@/server/blog';
+import RSS from 'rss';
+
+export async function GET(): Promise<Response> {
+  const rss = new RSS({
+    title: 'Jun.codes',
+    description: "Mats' blog & personal piece of the internet.",
+    categories: ['Software Development'],
+    site_url: 'https://jun.codes',
+    feed_url: 'https://jun.codes/rss.xml',
+    copyright: `© Mats Jun Larsen ${new Date().getFullYear()}`,
+    language: 'en-US',
+    image_url: 'https://jun.codes/snapshot.png',
+    pubDate: new Date(),
+    generator: 'Next.js',
+    docs: 'https://www.rssboard.org/rss-specification',
+  });
+  for (const post of await getAllBlogs(false)) {
+    rss.item({
+      title: post.title,
+      description: post.description,
+      url: `https://jun.codes/blog/${post.slug}`,
+      categories: post.tags,
+      date: post.date,
+      author: 'Mats Jun Larsen',
+    });
+  }
+  return new globalThis.Response(rss.xml(), {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
