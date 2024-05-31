2ceda59a94ec07c286eb9145f9d1ea07a3732c90
Mats Larsen <mats@jun.codes>
2024-05-31 23:56:07 +0200

    refactor: add social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,15 +24,39 @@ export default async function IndexPage({ searchParams }: PageParams) {
         <div className="flex w-full items-center gap-8">
           <div className="lg:w-2/3">
             <h1 className="font-bold text-2xl lg:text-4xl">
-              Software, Open Source & Bouldering
+              Software, Open Source, Travel & Bouldering
             </h1>
-            <p className="font-poppins">
+            <p className="font-poppins max-w-[80ch]">
               I'm Mats, a software engineer, open-source developer, and student
               based in Trondheim, Norway. I have a passion for building software
               people love. I'm currently pursing my Master's degree in
               informatics and artificial intelligence at the Norwegian
               University of Science and Technology.
             </p>
+            <address className="flex lg:hidden gap-2 items-center mt-2">
+              <a
+                href="https://linkedin.com/in/mats-jun-larsen"
+                className="border border-gray-6 rounded-md p-1 bg-white"
+              >
+                <Image
+                  src="/svg/linkedin.svg"
+                  alt="LinkedIn"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a
+                href="https://github.com/junlarsen"
+                className="border border-gray-6 rounded-md p-1 bg-white"
+              >
+                <Image
+                  src="/svg/github.svg"
+                  alt="GitHub"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </address>
           </div>
 
           <div className="hidden lg:block lg:w-1/3">
@@ -44,6 +68,30 @@ export default async function IndexPage({ searchParams }: PageParams) {
               height={741}
               priority
             />
+            <address className="gap-2 items-center mt-2 hidden lg:flex">
+              <a
+                href="https://linkedin.com/in/mats-jun-larsen"
+                className="border border-gray-6 rounded-md p-1 bg-white"
+              >
+                <Image
+                  src="/svg/linkedin.svg"
+                  alt="LinkedIn"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a
+                href="https://github.com/junlarsen"
+                className="border border-gray-6 rounded-md p-1 bg-white"
+              >
+                <Image
+                  src="/svg/github.svg"
+                  alt="GitHub"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </address>
           </div>
         </div>
       </Section>
@@ -52,9 +100,11 @@ export default async function IndexPage({ searchParams }: PageParams) {
         <h2 className="font-bold text-xl lg:text-2xl">
           {posts.length} post{posts.length === 1 ? '' : 's'}
         </h2>
-        <p className="font-poppins">
-          I'm a firm believer in knowledge sharing, and occasionally write about
-          software in my blog.
+        <p className="font-poppins max-w-[80ch]">
+          I firmly believe that the best way to understand is to teach. That's
+          why I love to talk about programming, software, and technology. I
+          occasionally write blog posts about things I find interesting, and I
+          hope you find them interesting too.
         </p>
 
         <div className="flex flex-col gap-4 mt-4">
