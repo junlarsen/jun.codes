f9d05a9f9de5c55017288c0e88dfc0c63214afaf
Mats Larsen <mats@jun.codes>
2024-05-31 22:00:08 +0200

    fix: try public environment variable

--- src/server/environment.ts
+++ src/server/environment.ts
@@ -8,4 +8,5 @@ declare global {
 
 export const isBetaMode =
   process.env.NODE_ENV === 'development' ||
-  process.env.VERCEL_URL === 'beta.jun.codes';
+  process.env.VERCEL_URL === 'beta.jun.codes' ||
+  process.env.NEXT_PUBLIC_VERCEL_URL	=== 'beta.jun.codes';
